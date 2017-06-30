b08538178c45cf210e4675d3038a043abb72c22d
Jason Ranjitkumar <jasontrk@hotmail.co.uk>
2017-06-30 16:28:59 +0100

    added seeds for entertainment and art & collectibles

---
+++ db/seeds.js
@@ -0,0 +1,113 @@
+{
+name: 'Human Skull Replica',
+category: 'Art & Collectibles',
+price: '23.00',
+quantity: '5',
+image: 'human-skull-replica'
+description: 'Realistic human skull replica, full size, made from plaster of Paris and painted for an aged, weathered appearance. The original and best selling Vicious Noodles skull, hundreds sold to satisfied customers. This item is hand-cast and hand-painted and is therefore a unique piece which will vary slightly in appearance from the photograph - the pictures give a close representation of the item you will receive.',
+createdBy:
+},{
+name: 'Pokemon Terrarium Pokeball',
+category: 'Art & Collectibles',
+price: '29.99',
+quantity: '10',
+image: 'pokemon-terrarium-pokeball'
+description: 'Pikachu pokemon terrarium. Size: The diameter is 3.93in (100mm). The balls will be sealed and do not require any maintenance and are great to have on a desk, at work, or at home.',
+createdBy:
+},{
+name: 'Skyrim Ebony Dagger',
+category: 'Art & Collectibles',
+price: '35.00',
+quantity: '8',
+image: 'skyrim-ebony-dagger'
+description: 'Wooden replica of the Ebony Dagger from Skyrim video game. This is an awesome looking item. The replica is made from wood so it is very lightweight making it perfect for cosplay and carying around conventions. The paint detail makes it look like metal. A great gift for any cosplay enthusiast or elder scrolls fan.',
+createdBy: []
+},{
+name: 'Scorpion Metal Sculpture',
+category: 'Art & Collectibles',
+price: '295.00',
+quantity: '3',
+image: 'scorpion-metal-sculpture'
+description: 'Scrap metal Scorpion sculpture. Made with a selection of waste metal and then welded together. They are approximately 11.5" long, 6.5" high and weigh approximately 1.7kg. I can make them to order from the same design but each sculpture will be unique due to the materials used. This sculpture is probably not suitable for children. The sting and the pincers are sharp!',
+createdBy:
+},{
+name: 'Stormtrooper Helmet',
+category: 'Art & Collectibles',
+price: '150.00',
+quantity: '5',
+image: 'stormtrooper-helmet'
+description: 'Fully wearable stormtrooper helmet hand painted and weathered by Custom Collectables. Comes with voice changer, adjustable head strap and helmet stand. If you want a particular colour finish or weathering material, please send me a message so we can arrange your boutique item.',
+createdBy:
+},{
+name: 'Muhammad Ali Print',
+category: 'Art & Collectibles',
+price: '30.00',
+quantity: '20',
+image: 'muhammad_ali_print'
+description: 'Muhammed Ali "I am the Greatest" print. Includes black frame with white mount.',
+createdBy:
+},{
+name: 'Orgone Pyramid',
+category: 'Art & Collectibles',
+price: '127.99',
+quantity: '5',
+image: ''
+description: 'This orgone pyramid is hand-made with loving intent. Everything is charged in the sunlight before encasing it in resin for maximum benefit. After it was made it was charged in the megalithic stone circle in lough Gur, Limerick, Ireland. It also sits on an orgone charging plate before posting. So it is full of spiritual and energetic energies. It contains chipped red carnelian gemstones, chipped gemstones, chipped sunstone gemstones, black tourmaline, moonstone tumblestones, gold flakes, terminated quartz crystal wrapped in copper wire, brass metal powder, copper metal shavings, brass metal shavings',
+createdBy:
+},{
+name: '5-tube IN14 Nixie clock',
+category: 'Entertainment',
+price: '130.50',
+quantity: '7',
+image: 'nixie-clock'
+description: 'Nixie Clock Features: All interaction using IR proximity sensors - no buttons or remotes. Provision for GPS time sync input 2400/4800/9600BPS. Data link can chain up to 3 clocks together into a single display. Single alarm. Display dim on/off settings. Time calibration. Individual tube brightness calibration. Ticking seconds. Low power, less than 2W and can be powered from a standard USB port. Approx. 80x145mm and 70mm high to the top of the tubes.',
+createdBy:
+},{
+name: 'Arduino LED Tetris Table',
+category: 'Entertainment',
+price: '175.05',
+quantity: '12',
+image: 'led-tetris-table'
+description: 'Arduino LED coffee table, with tetris game, beautiful bright LED illumination and cool random animations. This is a fully functional gaming console and cool coffee table. You can connect gamepad and play in retro game tetris or snake. You can use the table as a night light. Table size width: 55cm, length: 55cm, height: 45cm.',
+createdBy:
+},{
+name: 'Vintage Sony Stereo 8 Track Cartridge Recorder Model TC-8 ',
+category: 'Entertainment',
+price: '47.75',
+quantity: '1',
+image: 'sony-stereo-recorder'
+description: 'Sony Tapecorder TC-8 Stereo 8 Cartidge Recorder and 17 8-Tracks! Features include: Portable Built In Handle, Stereo Line Out, Stereo Auxillary In, Stereo Mic In, Plays & Records in Stereo',
+createdBy:
+},{
+name: 'VINTAGE mint Polaroid Sx-70 Land Camera',
+category: 'Entertainment',
+price: '151.51',
+quantity: '1',
+image: 'poloroid-camera'
+description: 'VINTAGE mint Polaroid Sx-70 Land Camera with tan leather body. It\'s in great condition! Bellows are in perfect condition and the glass is flawless',
+createdBy:
+},{
+name: 'Game Boy (built-in Hard Drive USB 3.0)',
+category: 'Entertainment',
+price: '87.50',
+quantity '5',
+image: 'gameboy'
+description: 'Go ultra portable and ultra geeky with this Game Boy handheld video game system, which has been recycled into a USB3 hard drive. This price is for a 1TB Drive.',
+createdBy:
+},{
+name: 'Steampunk Green Computer Mouse (USB)',
+category: 'Entertainment',
+price: '45.20',
+quantity: '5',
+image: 'steampunk-mouse'
+description: 'Armorhamster - custom modding computer mouse with armor, gears, rivets and mechanisms imitation at steampunk style. The mouse has a laser sensor with a change of resolution 600/1000/1600 dpi and additional navigation keys. On the back of the mouse located a window with a green light.',
+createdBy:
+},{
+name: 'Sega Megadrive (includes 16 games)',
+category: 'Entertainment',
+price: '132.00',
+quantity: '1',
+image: 'sega-megadrive'
+description: 'Console game Sega Megadrive with cables, controllers, 1 pad and 16 vintage games. Warning, this console is in Zone Europe. The games are in french. Good condition. Works perfectly. Tested before placing the ad online. One of the two levers has the open connector, but works perfectly.',
+createdBy:
+},{
