680feee5e8ce0a8b58876610ea2457ad827a218f
Jason Ranjitkumar <jasontrk@hotmail.co.uk>
2017-07-01 14:09:49 +0100

    updated frontend router.js and products.js

--- src/js/config/router.js
+++ src/js/config/router.js
@@ -1,15 +1,41 @@
 angular
-  .module('wabisabiApp')
-  .config(Router);
+.module('wabisabiApp')
+.config(Router);
 
 Router.$inject = ['$stateProvider', '$urlRouterProvider', '$locationProvider'];
 function Router($stateProvider, $urlRouterProvider, $locationProvider) {
   $locationProvider.html5Mode(true);
 
   $stateProvider
-    .state('productsIndex', {
-      url: '/products',
-      templateUrl: 'js/views/products/index.html'
-    });
-  $urlRouterProvider.otherwise('/');
+  .state('productsIndex', {
+    url: '/products',
+    templateUrl: 'js/views/products/index.html',
+    controller: 'ProductsIndexCtrl as productsIndex'
+  })
+  .state('productsNew', {
+    url: '/products/new',
+    templateUrl: 'js/views/products/new.html',
+    controller: 'BirdsNewCtrl as productsNew'
+  })
+  .state('productsShow', {
+    url: '/products/:id',
+    templateUrl: 'js/views/products/show.html',
+    controller: 'BirdsShowCtrl as productsShow'
+  })
+  .state('productsEdit', {
+    url: '/products/:id/edit',
+    templateUrl: 'js/views/products/edit.html',
+    controller: 'BirdsEditCtrl as productsEdit'
+  })
+  .state('login', {
+    url: '/login',
+    templateUrl: 'js/views/auth/login.html',
+    controller: 'LoginCtrl as login'
+  })
+  .state('register', {
+    url: '/register',
+    templateUrl: 'js/views/auth/register.html',
+    controller: 'RegisterCtrl as register'
+  });
+  $urlRouterProvider.otherwise('/products');
 }
