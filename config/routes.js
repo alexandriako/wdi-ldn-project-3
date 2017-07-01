1d9c43d2b3d13241384c515234cf851606f8863a
Mike de Groot <mike.j.degroot@gmail.com>
2017-07-01 13:59:11 +0100

    user routes up and running

--- config/routes.js
+++ config/routes.js
@@ -1,28 +1,28 @@
 const router = require('express').Router();
 const products = require('../controllers/products');
-// const users = require('../controllers/users');
+const users = require('../controllers/user');
 // const auth = require('../controllers/auth');
 // const secureRoute = require('../lib/secureRoute');
 
 
 router.route('/products/index')
-  .get(products.index)
-  .post(products.create);
+.get(products.index)
+.post(products.create);
 
 router.route('/products/:id')
-  // .all(secureRoute)
-  .get(products.show)
-  .put(products.update)
-  .delete(products.delete);
+// .all(secureRoute)
+.get(products.show)
+.put(products.update)
+.delete(products.delete);
 
-// router.route('/user/:id')
-//   .get(); //need to write a user controller
+router.route('/user/index')
+.get(users.index);
 
-// router.route('/register')
-//   .post(auth.register);
-//
-// router.route('/login')
-//   .post(auth.login);
+router.route('/user/:id')
+.get(users.show)
+.put(users.update)
+.delete(users.delete);
+
 
 
 
