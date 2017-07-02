c2b9265fe9858c90a86b6d32b2ca0201a90fee7d
Mike de Groot <mike.j.degroot@gmail.com>
2017-07-02 17:44:37 +0100

    no luck with stripe but ngcartroughly working

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,15 @@
 // app.js is the main JS file which you should define your Angular module
 console.log('Hot damn!');
-
+//
 angular
-  .module('wabisabiApp', ['ui.router', 'ngResource', 'satellizer', 'ui.bootstrap', 'ngAnimate'])
-  .constant('API_URL', 'http://localhost:7000')
-  .config(function() {
-    Stripe.setPublishableKey('pk_test_ULwvAlGf5PSFbkLqX9gQN8sA');
-  });
+.module('wabisabiApp', ['ui.router', 'ngResource', 'satellizer', 'ui.bootstrap', 'ngAnimate', 'ngCart'])
+// .constant('API_URL', 'http://localhost:4000')
+// .config(function() {
+//   Stripe.setPublishableKey('pk_test_ULwvAlGf5PSFbkLqX9gQN8sA');
+// });
+
+
+.controller('myCtrl', ['$scope', '$http', 'ngCart', function($scope, $http, ngCart) {
+  ngCart.setTaxRate(7.5);
+  ngCart.setShipping(2.99);
+}]);
