6c85a0c2f3fee7f913ca85e6d586ef10cac6ad2a
Mike de Groot <mike.j.degroot@gmail.com>
2017-07-02 17:53:38 +0100

    git bug fix second attempt

--- src/js/controllers/stripe.js
+++ src/js/controllers/stripe.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 angular
   .module('wabisabiApp')
   .controller('StripeController', StripeController)
@@ -53,53 +53,3 @@ function StripeController($http, Stripe) {
     document.getElementsByTagName('form')[0].reset();
   };
 }
-=======
-// angular
-//   .module('wabisabiApp')
-//   .controller('StripeCtrl', StripeCtrl);
-//
-// StripeCtrl.$inject = ['$http', 'Stripe'];
-// function StripeCtrl($http, Stripe) {
-//   var self = this;
-//
-//   self.card = {};
-//   self.payee = null;
-//   self.amount = null;
-//   self.currency = 'gbp';
-//   self.paymentSuccessful = true;
-//
-//   self.pay = function() {
-//     Stripe.card.createToken(self.card, function(status, response) {
-//       if(status === 200) {
-//         var data = {
-//           card: self.card,
-//           token: response.id,
-//           amount: self.amount,
-//           currency: self.currency,
-//           payee: self.payee
-//         };
-//
-//         $http
-//           .post(API_URL + '/payment', data)
-//           .then(function(res) {
-//             if(res.status === 200) {
-//               self.paymentSuccessful = true;
-//             } else {
-//               self.paymentSuccessful = false;
-//             }
-//           });
-//       }
-//     });
-//   };
-//
-//   self.reset = function() {
-//     self.card = {};
-//     self.payee = '';
-//     self.amount = null;
-//     self.paymentSuccessful = false;
-//     self.Form.$setPristine(true);
-//     // use vanilla JS to reset form to remove browser's native autocomplete highlighting
-//     document.getElementsByTagName('form')[0].reset();
-//   };
-// }
->>>>>>> development
