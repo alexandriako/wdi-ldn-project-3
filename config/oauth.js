af2723595f1beb740a25509a84cbf97d6d8918d4
Alexandria Okamoto <alexandriamhko@gmail.com>
2017-07-07 10:07:08 +0100

    instagra, oauth uri for heroku now

--- config/oauth.js
+++ config/oauth.js
@@ -2,7 +2,7 @@ module.exports = {
   instagram: {
     loginURL: 'https://api.instagram.com/oauth/authorize/',
     accessTokenURL: 'https://api.instagram.com/oauth/access_token',
-    redirect_uri: 'http://localhost:7000',
+    redirect_uri: 'https://wdi-27-honeypot.herokuapp.com/',
     clientId: process.env.INSTAGRAM_CLIENT_ID,
     clientSecret: process.env.INSTAGRAM_CLIENT_SECRET,
     scope: 'user:email'
