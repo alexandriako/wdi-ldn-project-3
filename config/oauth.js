5014902f668e4846a5ed6a680dde933accfbfafd
Alexandria Okamoto <alexandriamhko@gmail.com>
2017-07-19 11:33:25 +0100

    instagram oauth update

--- config/oauth.js
+++ config/oauth.js
@@ -3,8 +3,8 @@ module.exports = {
     loginURL: 'https://api.instagram.com/oauth/authorize/',
     accessTokenURL: 'https://api.instagram.com/oauth/access_token',
     redirect_uri: 'https://wdi-27-honeypot.herokuapp.com',
-    clientId: process.env.INSTAGRAM_CLIENT_ID,
-    clientSecret: process.env.INSTAGRAM_CLIENT_SECRET,
+    clientId: process.env.INSTAGRAM_CLIENT_ID_WABI_SABI,
+    clientSecret: process.env.INSTAGRAM_CLIENT_SECRET_WABI_SABI,
     scope: 'user:email'
   }
 };
