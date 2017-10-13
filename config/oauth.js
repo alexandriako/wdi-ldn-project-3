2bde6933e4fd8db3eae935c0c8afe7a81433b2d8
Alexandria Okamoto <alexandriamhko@gmail.com>
2017-10-13 16:22:08 +0200

    instagram oauth

--- config/oauth.js
+++ config/oauth.js
@@ -2,7 +2,7 @@ module.exports = {
   instagram: {
     loginURL: 'https://api.instagram.com/oauth/authorize/',
     accessTokenURL: 'https://api.instagram.com/oauth/access_token',
-    redirect_uri: 'https://wdi-27-honeypot.herokuapp.com',
+    redirect_uri: 'https://wabi-sabi.herokuapp.com',
     clientId: process.env.INSTAGRAM_CLIENT_ID_WABI_SABI,
     clientSecret: process.env.INSTAGRAM_CLIENT_SECRET_WABI_SABI,
     scope: 'user:email'
